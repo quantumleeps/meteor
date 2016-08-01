23e98b80669aad8b09e0794af707fc30f740e60d
quantumleeps <dan.leeper@gmail.com>
2016-08-01 19:59:19 -0400

    Connect to db and rename -change all references from party to plant -connect to mongodb database -create entry from console and render in browser

--- client/app.ts
+++ client/app.ts
@@ -1,32 +1,23 @@
 import 'reflect-metadata';
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
- 
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { Plants }   from '../collections/plants';
+import { Mongo }     from 'meteor/mongo';
+
+
 import template from './app.html';
  
 @Component({
   selector: 'app',
   template
 })
-class Plants { 
+class App { 
 
-    parties: Object[];
-
-    constructor() {
-        this.parties = [
-      {'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.',
-        'location': 'Palo Alto'
-      },
-      {'name': 'All dubstep all the time',
-        'description': 'Get it on!',
-        'location': 'Palo Alto'
-      },
-      {'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco'
-      }
-    ]; }
+    plants: Mongo.Cursor<Object>;
+ 
+  constructor () {
+    this.plants = Plants.find();
+  }
 }
  
-bootstrap(Plants);
+bootstrap(App);
---
+++ collections/plants.ts
@@ -0,0 +1,3 @@
+import {Mongo} from 'meteor/mongo';
+ 
+export let Plants = new Mongo.Collection('plants');
