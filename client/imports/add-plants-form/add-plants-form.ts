9e69073dbdd502d747a14815d5b7acd8d89980a3
quantumleeps <dan.leeper@gmail.com>
2016-08-01 20:42:51 -0400

    enable add plant input component

--- client/imports/add-plants-form/add-plants-form.ts
+++ client/imports/add-plants-form/add-plants-form.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import { Component } from '@angular/core';
+import { FormBuilder, ControlGroup, Validators, Control } from '@angular/common';
+import { Plants } from '../../../collections/plants.ts';
 
 import template from './add-plants-form.html';
 
@@ -7,4 +9,30 @@ import template from './add-plants-form.html';
   selector: 'add-plants-form',
   template
 })
-export class AddPlantsForm { }
+export class AddPlantsForm { 
+    addPlantsForm: ControlGroup;
+
+    constructor() {
+        let fb = new FormBuilder();
+
+        this.addPlantsForm = fb.group({
+            name: ['', Validators.required],
+            description: [''],
+            location: ['', Validators.required]
+        });
+    }
+
+    addPlant(plant) {
+        if (this.addPlantsForm.valid) {
+            Plants.insert({
+                name: plant.name,
+                description: plant.description,
+                location: plant.location
+            });
+
+            (<Control>this.addPlantsForm.controls['name']).updateValue('');
+            (<Control>this.addPlantsForm.controls['description']).updateValue('');
+            (<Control>this.addPlantsForm.controls['location']).updateValue('');
+        }
+    }
+}
